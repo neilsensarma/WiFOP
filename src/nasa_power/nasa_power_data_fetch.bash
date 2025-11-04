7ea6b261d4cd2c45a582ffbe54232b2c02f0c867
neilsensarma <nilavrasensarma@gmail.com>
2025-11-04 12:13:41 -0500

    downloaded all the raw data for the different parameters from nasa-power, merged them and restructured the files a bit.

---
+++ src/nasa_power/nasa_power_data_fetch.bash
@@ -0,0 +1,21 @@
+mkdir -p power_ca_jul2023 && cd power_ca_jul2023
+
+base='https://power.larc.nasa.gov/api/temporal/daily/regional'
+latmin=32.5; latmax=42.0
+start=20230701; end=20230731
+common="community=AG&latitude-min=$latmin&latitude-max=$latmax&start=$start&end=$end&time-standard=UTC&format=CSV"
+
+vars=(T2M RH2M WS10M PRECTOT PS)
+
+tiles=("-124.5 -119.5" "-119.5 -114.1")
+
+for v in "${vars[@]}"; do
+  i=1
+  for tile in "${tiles[@]}"; do
+    read lonmin lonmax <<<"$tile"
+    url="$base?parameters=$v&longitude-min=$lonmin&longitude-max=$lonmax&$common"
+    curl -L "$url" -o "${v}_tile${i}.csv"
+    ((i++))
+    sleep 0.5
+  done
+done
