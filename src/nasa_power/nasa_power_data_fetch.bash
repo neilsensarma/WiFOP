1182f02c59a9623742ffc0655e01d2a99301c570
neilsensarma <nilavrasensarma@gmail.com>
2025-11-24 19:50:56 -0500

    nasa power for 3 months downloaded

--- src/nasa_power/nasa_power_data_fetch.bash
+++ src/nasa_power/nasa_power_data_fetch.bash
@@ -1,8 +1,8 @@
-mkdir -p power_ca_jul2023 && cd power_ca_jul2023
+mkdir -p power_ca_jul_sep2023 && cd power_ca_jul_sep2023
 
 base='https://power.larc.nasa.gov/api/temporal/daily/regional'
 latmin=32.5; latmax=42.0
-start=20230701; end=20230731
+start=20230701; end=20230930
 common="community=AG&latitude-min=$latmin&latitude-max=$latmax&start=$start&end=$end&time-standard=UTC&format=CSV"
 
 vars=(T2M RH2M WS10M PRECTOT PS)
